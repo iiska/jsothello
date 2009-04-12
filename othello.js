26cffcdf2d8ac6ed74781bba8c78624e7ccbfb02
Juhamatti Niemelä <iiska@iki.fi>
2009-04-12 19:43:57 +0300

    Working two player hotseat game.

--- othello.js
+++ othello.js
@@ -1,18 +1,23 @@
 Ext.namespace('Ext.iiska');
 
 Ext.iiska.OthelloBoard = function(element, forceNew) {
-    Ext.iiska.OthelloBoard.superclass.constructor.call(this, element, forceNew);
+    Ext.iiska.OthelloBoard.superclass.constructor.call(this,
+        element, forceNew);
 
     var dh = Ext.DomHelper;
     this.gridEl = dh.overwrite(this, {tag:'div', cls:'grid'}, true);
-    dh.append(this.gridEl, {tag:'p', cls:'border', html:'╔═╤═╤═╤═╤═╤═╤═╤═╗'});
+    dh.append(this.gridEl,
+              {tag:'p', cls:'border', html:'╔═╤═╤═╤═╤═╤═╤═╤═╗'});
     for(var i=0;i<8;++i) {
         if (i > 0) {
-            dh.append(this.gridEl, {tag:'p', cls:'border', html:'╟─┼─┼─┼─┼─┼─┼─┼─╢'});
+            dh.append(this.gridEl,
+                      {tag:'p', cls:'border', html:'╟─┼─┼─┼─┼─┼─┼─┼─╢'});
         }
-        dh.append(this.gridEl, {tag:'p', cls:'row row'+i, html:'║ │ │ │ │ │ │ │ ║'});
+        dh.append(this.gridEl,
+                  {tag:'p', cls:'row row'+i, html:'║ │ │ │ │ │ │ │ ║'});
     }
-    dh.append(this.gridEl, {tag:'p', cls:'border', html:'╚═╧═╧═╧═╧═╧═╧═╧═╝'});
+    dh.append(this.gridEl,
+              {tag:'p', cls:'border', html:'╚═╧═╧═╧═╧═╧═╧═╧═╝'});
     dh.append(this, {tag:'div', cls:'score', cn: [
                          {tag:'p', cls:'score', html:'White: 2, Black: 2'}
                      ]});
@@ -24,40 +29,83 @@ Ext.iiska.OthelloBoard = function(element, forceNew) {
 };
 
 Ext.extend(Ext.iiska.OthelloBoard, Ext.Element, {
-               gridEl: undefined,
+    gridEl: undefined,
 
-               setMove: function(x, y, piece) {
-                   var row = Ext.DomQuery.selectNode('p.row'+y, this.dom);
-                   if (row) {
-                       row.innerHTML = row.innerHTML.substring(0,2*x+1) + piece +
-                           row.innerHTML.substring(2*x+2);
-                   }
-               },
+    setMove: function(x, y, piece, turn) {
+        var score;
+        var row = Ext.DomQuery.selectNode('p.row'+y, this.dom);
+        if (row) {
+            row.innerHTML = row.innerHTML.substring(0,2*x+1) + piece +
+                row.innerHTML.substring(2*x+2);
+        }
+        if (turn) {
+            score = this.turnPieces(x, y, piece);
+        }
+        return score;
+    },
 
-               getMove: function(x, y) {
-                   var row = Ext.DomQuery.selectNode('p.row'+y, this.dom);
-                   if (row) {
-                       return row.innerHTML.charAt(2*x+1);
-                   }
-                   return null;
-               },
+    turnPieces: function(x, y, piece, simulate) {
+        var dirs = [
+            [1,1], [-1,1], [1,-1], [-1,-1],
+            [1,0], [-1,0], [0, 1], [0, -1]
+        ];
+        var pieces = [];
+        Ext.each(dirs, function(i) {
+            var cx = x; var cy = y; var m;
+            var tmp_pieces = [];
+            for(;;) {
+                cx += i[0]; cy += i[1];
+                m = this.getMove(cx, cy);
+                if ( (m === null) || (m === ' ') ) {
+                    break;
+                } else if (m != piece) {
+                    tmp_pieces.push([cx, cy]);
+                } else if (m === piece) {
+                    pieces = pieces.concat(tmp_pieces);
+                    break;
+                }
+            }
+        }, this);
+        if (!simulate) {
+            Ext.each(pieces, function(i) {
+                this.setMove(i[0], i[1], piece, false);
+            }, this);
+        }
+        return pieces.length;
+    },
 
-               getRow: function(y) {
-                   var row = Ext.DomQuery.selectNode('p.row'+y, this.dom);
-                   if (row) {
-                       return row.innerHTML;
-                   }
-                   return null;
-               }
-           });
+    getMove: function(x, y) {
+        var row = Ext.DomQuery.selectNode('p.row'+y, this.dom);
+        if (row && (x >= 0) && (x < 8) ) {
+            return row.innerHTML.charAt(2*x+1);
+        }
+        return null;
+    },
+
+    getRow: function(y) {
+        var row = Ext.DomQuery.selectNode('p.row'+y, this.dom);
+        if (row) {
+            return row.innerHTML;
+        }
+        return null;
+    },
+
+    updateScore: function(score) {
+        var s = Ext.DomQuery.selectNode("p.score", this.dom);
+        s.innerHTML = "White: " + score.white + ", Black: " + score.black;
+    }
+});
 
 Ext.namespace("Othello");
-Othello.Player = function() {
-    return {};
+
+Othello.Player = function() {};
+Othello.Player.prototype = {
+    score: 2
 };
 
-Othello.HumanPlayer = function() {
+Othello.HumanPlayer = function(config) {
     Othello.HumanPlayer.superclass.constructor.call(this);
+    this.piece = config.piece;
 };
 Ext.extend(Othello.HumanPlayer, Othello.Player, {});
 
@@ -67,28 +115,39 @@ Othello.AIWrapper = function() {
 Ext.extend(Othello.AIWrapper, Othello.Player, {});
 
 Othello.Game = function(config) {
-    var currentPlayer;
-    var board = config.board;
-    var logEl = config.logEl;
+    this.playerIndex = 0;
+    this.players = [new Othello.HumanPlayer({piece: '○'}),
+                  new Othello.HumanPlayer({piece: '●'})];
+    this.currentPlayer = this.players[this.playerIndex];
+    this.board = config.board;
+    this.logEl = config.logEl;
 
-    var rows = Ext.DomQuery.select("p.row", board.dom);
+    var rows = Ext.DomQuery.select("p.row", this.board.dom);
     Ext.each(rows, function(i) {
-                 Ext.fly(i).on("click", function(event, node) {
-                                   var y = parseInt(node.className.match(/([0-9]+)/)[1],10);
-                                   // Depends heavily on font-size, but using
-                                   // single string without span elements makes the
-                                   // DOM a lot lighter.
-                                   var x = Math.floor((event.browserEvent.layerX-
-                                                       node.offsetLeft) / 14);
-                                   if (x<0) {x=0;}
-                                   else if (x>7) {x=7;}
-                                   board.setMove(x,y,'o');
-                               }, this);
-             }, this);
+        Ext.fly(i).on("click", function(event, node) {
+            if (!(this.currentPlayer instanceof Othello.HumanPlayer)) {
+                return false;
+            }
+            var y = parseInt(node.className.match(/([0-9]+)/)[1],10);
+            // Depends heavily on font-size, but using
+            // single string without span elements makes the
+            // DOM a lot lighter.
+            var x = Math.floor((event.browserEvent.layerX-
+                node.offsetLeft) / 14);
+            if (x<0) {x=0;}
+            else if (x>7) {x=7;}
 
-    return {
-        //board: undefined,
-        //logEl: undefined,
+            this.makeMove(x,y,this.currentPlayer.piece);
+        }, this);
+    }, this);
+};
+
+Othello.Game.prototype = {
+    playerIndex: undefined,
+    currentPlayer: undefined,
+    players: undefined,
+    board: undefined,
+    logEl: undefined,
 
         getInterestingArea: function() {
             var r, i;
@@ -141,16 +200,28 @@ Othello.Game = function(config) {
         },
 
         isLegalMove: function(x, y, piece) {
-            var my = piece;
-            var opp = (my === '○') ? '●' : '○';
-
-            if (board.getMove(x,y) !== ' ') {
+            if (this.board.getMove(x,y) !== ' ') {
                 return false;
+            } else if (this.board.turnPieces(x,y,piece,true) > 0) {
+                return true;
             }
+            return false;
+        },
 
-            function next() {
+        makeMove: function(x, y, piece) {
+            if (this.isLegalMove(x,y,piece)) {
+                var score = this.board.setMove(x, y, piece, true);
+                this.players[this.playerIndex].score += score + 1;
+                this.players[this.playerIndex^1].score -= score;
+                this.currentPlayer = this.players[this.playerIndex ^= 1];
+                this.board.updateScore({
+                    white: this.players[0].score,
+                    black: this.players[1].score
+                });
+                this.log("Player with " +
+                         this.currentPlayer.piece +
+                         " pieces, make your move.");
             }
-            return true;
         },
 
         getPossibleMoves: function() {
@@ -158,9 +229,8 @@ Othello.Game = function(config) {
         },
 
         log: function(str) {
-            if (logEl) {
-                Ext.DomHelper.append(logEl, {tag:'p', html:str});
+            if (this.logEl) {
+                Ext.DomHelper.append(this.logEl, {tag:'p', html:str});
             }
         }
     };
-};
